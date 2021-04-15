e787661e7f542a293daf9bd578eb5ee9ebddfe5a
Kristian Nygaard Jensen <kristian.nygaard.jensen@coop.dk>
2021-04-15 12:56:09 +0200

    update yaml with list

--- list2yaml/main.tf
+++ list2yaml/main.tf
@@ -1,10 +1,18 @@
 locals {
-  list = [
-    "hest",
-    "hjort"
+  elastic_out_namespaces = [
+    "cattle-logging-system",
+    "default",
+    "ingress-nginx"
   ]
 }
 
+data "template_file" "cluster_flow" {
+  template = file("${path.module}/manifest.yaml")
+  vars = {
+    excluded_namespaces = join("\n", formatlist("        - %s", tolist(local.elastic_out_namespaces)))
+  }
+}
+
 output "yaml" {
-  value = yamlencode(local.list)
+  value = data.template_file.cluster_flow.rendered
 }
